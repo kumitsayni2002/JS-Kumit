2f6566123f6ed31ffc326a5d13e173c5b265ab62
kumitsayni2002 <kumitsayni732@gmail.com>
2025-01-11 09:52:26 +0000

    conversion of deta type

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,12 @@
+let score ="354ss"
+console.log( typeof score);
+console.log(typeof (score));
+let ValueInNumber =Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+//"33"  =>33
+//"33dds" =>NaN
+//"true"  =>1 ,"folces" =>0
+//undifine  =>0
