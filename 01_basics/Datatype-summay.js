3f1d487c101d45d3ea47e419f522d24c1b03ce94
kumitsayni2002 <kumitsayni732@gmail.com>
2025-01-12 08:34:31 +0000

    datatype-summry

---
+++ 01_basics/Datatype-summay.js
@@ -0,0 +1,15 @@
+// data type in two type 1 primitive , 2 non primitive
+//primitivein 7-type:-Number,null,symbol,string,boolean, undifined,BigInt
+
+ const score=300
+ const scoreValue=300.5
+ const isLoggednId=false
+ 
+const OutsideTemp=null
+const Id ="55855"
+const anotherId=symbol('24555')
+console.log(Id===anotherId);
+
+
+//Non-Primitive Data Types(Referencs):-Objective ,Array, function
+//https://ecma-international.org/5.1/#Sec11.4.3
